ce3c616e121717c2eb5f1fa43b6a953feb604e97
Nurdoka <nurdoolot93@gmail.com>
2023-07-09 20:42:45 -0500

    obj2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    int year = 1990;
+    String month = "July";
+    boolean weekend = true;
+
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(weekend); 
+  }
+}
